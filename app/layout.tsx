18fc65fb56526907e33b60e516e8317b26f2a8be
ryandarealest <caovanluan8@gmail.com>
2026-06-07 13:08:21 +0700

    Update preview image, hamburger

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,28 +22,28 @@ export const metadata: Metadata = {
     "UAE distributors",
   ],
   openGraph: {
-    title: "Delta Prime Global",
-    description: site.description,
-    url: site.url,
-    siteName: site.name,
-    locale: "en_US",
-    type: "website",
+  title: "Delta Prime Global",
+  description: site.description,
+  url: site.url,
+  siteName: site.name,
+  locale: "en_US",
+  type: "website",
+  images: [
+    {
+      url: `${site.url}/images/preview.png`,
+      width: 1200,
+      height: 630,
+      alt: "Delta Prime Global",
+    },
+  ],
+},
 
-    images: [
-      {
-        url: "/images/preview.png",
-        width: 1200,
-        height: 630,
-        alt: "Delta Prime Global",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Delta Prime Global",
-    description: site.description,
-    images: ["/images/preview.png"],
-  },
+twitter: {
+  card: "summary_large_image",
+  title: "Delta Prime Global",
+  description: site.description,
+  images: [`${site.url}/images/preview.png`],
+},
 
   icons: {
     icon: "/images/logoexport.png",
